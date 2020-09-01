9898b2da57895bbd9eceadce86de201e40a88a85
VarunRaj7 <rvarunraj1@gmail.com>
2020-08-31 23:06:59 -0400

    adding getusersessions

---
+++ backend/src/lambda/http/GetUserSessions.ts
@@ -0,0 +1,53 @@
+import 'source-map-support/register';
+
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyHandler,
+  APIGatewayProxyResult,
+} from 'aws-lambda';
+
+import { createLogger } from '../../utils/logger';
+
+import { UserAccess } from '../datalayer/UserdDBAcceess';
+import { SessionAccess } from '../datalayer/SessionDBAccess';
+
+// import { parseUserId } from '../../auth/utils';
+// import { CreateUserReq } from '../../models/CreateUserReq';
+
+const logger = createLogger('InsertUserDB');
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  logger.info(`create request for ${JSON.stringify(event.body)} received!!`);
+
+  //   const token: string = event.headers.Authorization.split(' ')[1];
+
+  //   const userId = await parseUserId(token);
+
+  //   const userdetails: CreateUserReq = JSON.parse(event.body);
+  //userId: userId,
+
+  const userId = event.headers.userId;
+
+  logger.info(`${userId}`);
+
+  const ItemResponse = await new UserAccess().getUser(userId);
+
+  var sessions: Array<string> = new Array();
+
+  const sessiondbObj = new SessionAccess();
+
+  for (let sessionId of JSON.parse(ItemResponse.results).history) {
+    sessions.push((await sessiondbObj.getSession(sessionId)).results);
+  }
+
+  return {
+    statusCode: ItemResponse.status,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify(sessions),
+  };
+};
