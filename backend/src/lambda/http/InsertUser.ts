38d6a24303d41bc4167d91bde4a3e568f74851ba
VarunRaj7 <rvarunraj1@gmail.com>
2020-08-31 21:01:16 -0400

    adding insert apis

---
+++ backend/src/lambda/http/InsertUser.ts
@@ -0,0 +1,46 @@
+import 'source-map-support/register';
+
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyHandler,
+  APIGatewayProxyResult,
+} from 'aws-lambda';
+
+import { createLogger } from '../../utils/logger';
+
+import { UserItem } from '../../models/UserItem';
+import { UserAccess } from '../datalayer/UserdDBAcceess';
+
+// import { parseUserId } from '../../auth/utils';
+// import { CreateUserReq } from '../../models/CreateUserReq';
+
+const logger = createLogger('InsertUserDB');
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  logger.info(`create request for ${JSON.stringify(event.body)} received!!`);
+
+  //   const token: string = event.headers.Authorization.split(' ')[1];
+
+  //   const userId = await parseUserId(token);
+
+  //   const userdetails: CreateUserReq = JSON.parse(event.body);
+  //userId: userId,
+
+  const userdetails: UserItem = JSON.parse(event.body);
+
+  const ItemResponse = await new UserAccess().createUser({
+    createdAt: new Date().toISOString(),
+    ...userdetails,
+  });
+
+  return {
+    statusCode: ItemResponse.status,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: ItemResponse.results,
+  };
+};
