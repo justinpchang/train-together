b55198d1a5a586f7185e9cd4ddd6663b329c739c
VarunRaj7 <rvarunraj1@gmail.com>
2020-08-31 21:23:19 -0400

    added get functions

---
+++ backend/src/lambda/http/GetSession.ts
@@ -0,0 +1,42 @@
+import 'source-map-support/register';
+
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyHandler,
+  APIGatewayProxyResult,
+} from 'aws-lambda';
+
+import { createLogger } from '../../utils/logger';
+
+import { SessionAccess } from '../datalayer/SessionDBAccess';
+
+// import { parseUserId } from '../../auth/utils';
+// import { CreateUserReq } from '../../models/CreateUserReq';
+
+const logger = createLogger('InserSessionDB');
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  logger.info(`create request for ${JSON.stringify(event.body)} received!!`);
+
+  //   const token: string = event.headers.Authorization.split(' ')[1];
+
+  //   const userId = await parseUserId(token);
+
+  //   const userdetails: CreateUserReq = JSON.parse(event.body);
+  //userId: userId,
+
+  const sessionId = JSON.parse(event.body);
+
+  const ItemResponse = await new SessionAccess().getSession(sessionId);
+
+  return {
+    statusCode: ItemResponse.status,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: ItemResponse.results,
+  };
+};
