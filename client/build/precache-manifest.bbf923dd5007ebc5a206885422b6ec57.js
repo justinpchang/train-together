9ee75b1e4581ed18bd8a762fe24488a59c52620a
Jonathan Yates <carlock1609@gmail.com>
2020-09-02 11:01:19 -0700

    testing build

---
+++ client/build/precache-manifest.bbf923dd5007ebc5a206885422b6ec57.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98d2e884f5497131b9e15358143d1e88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "998a5741fe49cc06bb13",
+    "url": "/static/css/main.cf2978a6.chunk.css"
+  },
+  {
+    "revision": "0e8bb452ab92c3f7f703",
+    "url": "/static/js/2.e449d8d7.chunk.js"
+  },
+  {
+    "revision": "729937898630e839994162d5d9f8ce89",
+    "url": "/static/js/2.e449d8d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "998a5741fe49cc06bb13",
+    "url": "/static/js/main.f2a8f871.chunk.js"
+  },
+  {
+    "revision": "0231f95ac667b73c9187",
+    "url": "/static/js/runtime-main.91e21c86.js"
+  },
+  {
+    "revision": "1b12ad81f082f86b80e9b3aa68a94de4",
+    "url": "/static/media/boxing.1b12ad81.png"
+  },
+  {
+    "revision": "c1c03c96f5b58073ca28b5da9625e2f7",
+    "url": "/static/media/class-modal-thumbnail.c1c03c96.png"
+  },
+  {
+    "revision": "5df0a910e4e3b7d60423afbd201b3e46",
+    "url": "/static/media/core.5df0a910.png"
+  },
+  {
+    "revision": "698a5191b52245ad24dfd3c8c4326f65",
+    "url": "/static/media/dance.698a5191.png"
+  },
+  {
+    "revision": "a5369809569edc4e18ad64c495326f6e",
+    "url": "/static/media/default-profile-picture.a5369809.png"
+  },
+  {
+    "revision": "fcee55cc3f8cfa722b8b7eeac06ed510",
+    "url": "/static/media/hero-image-transparent-3.fcee55cc.png"
+  },
+  {
+    "revision": "b7d98efd006d9bdc0c79dad7970d4165",
+    "url": "/static/media/logo1.b7d98efd.png"
+  },
+  {
+    "revision": "641d72a9e158c6135598be742e026bfb",
+    "url": "/static/media/other.641d72a9.png"
+  },
+  {
+    "revision": "95aa4e27126c4d0b749b129883a6076e",
+    "url": "/static/media/user-profile.95aa4e27.png"
+  },
+  {
+    "revision": "c5e6b7ede07dd457c50a0c8484811a42",
+    "url": "/static/media/weight.c5e6b7ed.png"
+  },
+  {
+    "revision": "1a75ee66b46299c9950331eab6a548bc",
+    "url": "/static/media/yoga.1a75ee66.png"
+  }
+]);
